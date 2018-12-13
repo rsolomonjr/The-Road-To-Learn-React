28409bbdeb4c30a384a0ec1ed65532c4972b8890
Rodney Solomon <rodneysolomonjr@gmail.com>
2018-12-13 14:42:04 -0500

    ReactJS Table Beta Build - w/ Static Data

--- hackernews-renewed/src/App.js
+++ hackernews-renewed/src/App.js
@@ -64,49 +64,118 @@ class App extends Component {
     const {searchTerm, list} = this.state;
     return (
       <div className="App">
-
-      {/* leanpub-start-insert */}
-      <tr className="tableHeader"><th colspan="5">REACTJS TOOLS</th></tr>
-      {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
-          const onHandleDismiss = () =>
-          this.onDismiss(item.objectID);
-          return (
-          <tr key={item.objectID}>
-             <td className="Book_Title">
-               <a href={item.url}>{item.title}</a>
-             </td>
-             <td className="Author">{item.author}</td>
-             <td className="Comments">{item.num_comments}</td>
-             <td className="Points">{item.points}</td>
-             <td>
-             <button
-               className = {`btn`}
-               onClick={onHandleDismiss}
-               type="button">
-               Remove Item
-             </button>
-             </td>
-           </tr>
-            );
-          }
-        )}
-      {/* leanpub-end-insert */}
-      <tr id="SearchForm">
-        <th colspan="5"><h2 className="SearchForm_Header">Search the List</h2>
-
-        <input
-          type="text"
+        <tr className="tableHeader">
+          <th colspan="5">REACTJS TOOLS</th>
+          </tr>
+        <tr>
+          <td className="tableHeader-ruler" colspan="5">
+          </td>
+        </tr>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+        <Search
           value={searchTerm}
           onChange={this.onSearchChange}
+        />
+      </div>
+    );
+  }
+}
+
+const Search = ({ value, onChange }) =>
+    <div>
+      <tr id="SearchForm">
+        <th colspan="5">
+          <h2 className="SearchForm_Header">Search the List</h2>
+          <input
+            type="text"
+            value={value}
+            onChange={onChange}
+            pattern="^[a-zA-Z]+$"
+            placeholder="Enter Title/Letters Here"
+          />
+          <div className="footnote">
+            <em>** Do not enter numbers</em>
+          </div>
+        </th>
+      </tr>
+    </div>
+{/*
+class Search extends Component {
+  render (){
+    const {value, onChange, children} = this.props;
+    return (
+      <div>
+      <tr id="SearchForm">
+        <th colspan="5"><h2 className="SearchForm_Header">Search the List</h2>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
           pattern="^[a-zA-Z]+$"
+          placeholder="Enter Title/Letters Here"
         />
         <div className="footnote"><em>** Do not enter numbers</em></div>
         </th>
       </tr>
       </div>
+    );
+  }
+}
+*/}
 
+class Table extends Component {
+  render () {
+    const {list, pattern, onDismiss} = this.props;
+    return (
+      <div>
+        {/* START OF THE MAIN APP */}
+        {list.filter(isSearched(pattern)).map(item => {
+            const onHandleDismiss = () =>
+            this.onDismiss(item.objectID);
+            return (
+            <tr key={item.objectID} id="Tools">
+               <td className="Book_Title">
+                 <a href={item.url}>{item.title}</a>
+               </td>
+               <td className="Author">{item.author}</td>
+               <td className="Comments">{item.num_comments}</td>
+               <td className="Points">{item.points}</td>
+               <td>
+               <Button onClick={() => onDismiss(item.objectID)}>
+                 Remove Item
+               </Button>
+               </td>
+             </tr>
+              );
+            }
+          )}
+        {/* END OF THE MAIN APP */}
+      </div>
     );
   }
 }
 
+class Button extends Component {
+  render () {
+    const {
+      onClick,
+      className = "btn",
+      children
+    } = this.props;
+
+    return (
+      <button
+        onClick = {onClick}
+        className = {className}
+        type="button"
+        >
+          {children}
+        </button>
+    );
+  }
+}
 export default App;
