bdb4c0d89e5385ddf6e91dff3dc48e415da7c7f8
Rodney Solomon <rodneysolomonjr@gmail.com>
2018-12-14 14:39:09 -0500

    ReactJS Table Beta Build - Hackernews API

--- hackernews-renewed/src/App.js
+++ hackernews-renewed/src/App.js
@@ -1,134 +1,191 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// # leanpub-start-insert
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Parcel',
-    url: 'https://parceljs.org/',
-    author: 'Devon Govett',
-    num_comments: 8,
-    points: 12,
-    objectID: 2,
-  },
-];
-// # leanpub-end-insert
-function isSearched(searchTerm){
-  return function(item){
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }
-}
+//# API CODING
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '25';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
 
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const {searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  fetchSearchTopStories(searchTerm, page=0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => this.setState({ e }));
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange (e) {
     this.setState({ searchTerm: e.target.value });
   }
 
+  onSearchSubmit (e) {
+    const {searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    e.preventDefault();
+  }
+
   onDismiss(id){
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
   }
 
   render() {
-    const {searchTerm, list} = this.state;
+    const {
+          searchTerm,
+          results,
+          searchKey,
+          error
+        } = this.state;
+
+        const page = (
+          results &&
+          results[searchKey] &&
+          results[searchKey].page
+        ) || 0;
+
+        if (error) {
+          return <p>Kittens died. Try again.</p>;
+        }
+
+        const list = (
+          results &&
+          results[searchKey] &&
+          results[searchKey].hits
+        ) || [];
+
     return (
+      <div className="table">
       <div className="App">
         <tr className="tableHeader">
-          <th colspan="5">REACTJS TOOLS</th>
+          <th colSpan={5}>REACTJS TOOLS</th>
           </tr>
         <tr>
-          <td className="tableHeader-ruler" colspan="5">
+          <td className="tableHeader-ruler" colSpan={5}>
           </td>
         </tr>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        />
+          { results ?
+            <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
+            : null
+          }
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          />
+      </div>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange }) =>
-    <div>
-      <tr id="SearchForm">
-        <th colspan="5">
+const Table = ({ list, onDismiss }) =>
+<div>
+  {/* START OF THE MAIN APP */}
+  {list.map(item =>
+      <tr key={item.objectID} id="Tools">
+         <td className="Book_Title">
+           <a href={item.url}>{item.title}</a>
+         </td>
+         <td className="Author">{item.author}</td>
+         <td className="Comments">{item.num_comments}</td>
+         <td className="Points">{item.points}</td>
+         <td>
+         <Button onClick={() => onDismiss(item.objectID)}>
+           Remove Item
+         </Button>
+         </td>
+       </tr>
+    )}
+  {/* END OF THE MAIN APP */}
+</div>
+
+const Search = ({ value, onChange, onSubmit }) =>
+      <form id="SearchForm" onSubmit={onSubmit}>
           <h2 className="SearchForm_Header">Search the List</h2>
           <input
             type="text"
             value={value}
             onChange={onChange}
-            pattern="^[a-zA-Z]+$"
             placeholder="Enter Title/Letters Here"
           />
+          <button type="submit" className="btn">
+            Submit
+          </button>
           <div className="footnote">
             <em>** Do not enter numbers</em>
           </div>
-        </th>
-      </tr>
-    </div>
-
-class Table extends Component {
-  render () {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-        {/* START OF THE MAIN APP */}
-        {list.filter(isSearched(pattern)).map(item =>
-            <tr key={item.objectID} id="Tools">
-               <td className="Book_Title">
-                 <a href={item.url}>{item.title}</a>
-               </td>
-               <td className="Author">{item.author}</td>
-               <td className="Comments">{item.num_comments}</td>
-               <td className="Points">{item.points}</td>
-               <td>
-               <Button onClick={() => onDismiss(item.objectID)}>
-                 Remove Item
-               </Button>
-               </td>
-             </tr>
-          )}
-        {/* END OF THE MAIN APP */}
-      </div>
-    );
-  }
-}
+      </form>
 
 class Button extends Component {
   render () {
